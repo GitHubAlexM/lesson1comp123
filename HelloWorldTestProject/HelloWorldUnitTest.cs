855d23a0c1c9a048488a0359cdc3cfe6a656adfd
GitHubAlexM <m.alexander1996@live.ca>
2018-03-05 20:50:31 -0500

    tests work

---
+++ HelloWorldTestProject/HelloWorldUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorldTestProject
+{
+    [TestClass]
+    public class HelloWorldUnitTest
+    {
+        [TestMethod]
+        public void HelloWorldToConsoleTestMethod()
+        {
+            // Arrange 
+            //string wrongExpectedResult = "Hello World";
+            ////Should fail
+
+            string correctExpectedResult = "Hello, World!";
+            //Should pass
+
+            // Act
+            string result = Lesson1.Program.HelloWorldToConsole();
+
+            // Assert
+            //Assert.AreEqual(wrongExpectedResult, result);
+            Assert.AreEqual(correctExpectedResult, result);
+        }
+    }
+}
