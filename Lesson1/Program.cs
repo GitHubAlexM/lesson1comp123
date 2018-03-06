36a6536c58d210eb4fa248a2d0ece750318a3eb8
GitHubAlexM <m.alexander1996@live.ca>
2018-03-05 20:38:14 -0500

    added in code

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -10,6 +10,20 @@ namespace Lesson1
     {
         static void Main(string[] args)
         {
+            //calling @method HelloWorldToConsole
+            HelloWorldToConsole();
+        }
+
+        /*
+         * @description This is my Hello World method
+         * @method HelloWorldToConsole
+         * @returns {string} HelloWorldString
+         */
+        public static string HelloWorldToConsole()
+        {
+            string HelloWorldString = "Hello, World!";
+            Console.WriteLine(HelloWorldString);
+            return HelloWorldString;
         }
     }
 }
